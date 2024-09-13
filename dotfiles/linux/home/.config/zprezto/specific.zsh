7a9f3ab2d4a6f80b82eec3df64611e97375bc247
Ziheng Zhuang <neoclear@outlook.com>
2024-09-12 22:10:39 -0700

    .

--- dotfiles/linux/home/.config/zprezto/specific.zsh
+++ dotfiles/linux/home/.config/zprezto/specific.zsh
@@ -3,3 +3,7 @@ xcape -e 'Control_L=Escape'
 
 source /usr/share/doc/fzf/examples/key-bindings.zsh
 source /usr/share/doc/fzf/examples/completion.zsh
+
+alias pbcopy='xclip -selection clipboard'
+
+export XSECURELOCK_COMPOSITE_OBSCURER=0

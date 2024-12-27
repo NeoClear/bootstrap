4cadc2c1c5bd3d30c9cb8054750334f52e2473b3
NeoClear <neoclear@outlook.com>
2024-12-27 00:14:06 -0800

    .

--- dotfiles/common/home/.config/zprezto/common.zsh
+++ dotfiles/common/home/.config/zprezto/common.zsh
@@ -1,2 +1,15 @@
 [[ ! -r ~/.opam/opam-init/init.zsh ]] || source ~/.opam/opam-init/init.zsh  > /dev/null 2> /dev/null
 [ -f "$HOME/.cargo/env" ] && source "$HOME/.cargo/env"
+eval "$(zoxide init zsh)"
+
+# Check if the ~/.zshrc.d directory exists
+if [[ -d $HOME/.zshrc.d ]]; then
+    # Loop through all files in the directory
+    for config_file in $HOME/.zshrc.d/*.zsh(N); do
+        # Check if the file is readable
+        if [[ -r $config_file ]]; then
+            # Source the file
+            source $config_file
+        fi
+    done
+fi
